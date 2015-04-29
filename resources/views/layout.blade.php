0ffe9ae3a5f208ab17d701a8f8566f0241838592
Chris Riebschlager <riebschlager@gmail.com>
2015-04-28 21:51:25 -0500

    cloak

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,8 +9,9 @@
     <script src="//use.typekit.net/pws0aeg.js"></script>
     <script>try{Typekit.load();}catch(e){}</script>
     <link rel="stylesheet" href="css/main.css">
+    <style>[ng\:cloak], [ng-cloak], [data-ng-cloak], [x-ng-cloak], .ng-cloak, .x-ng-cloak {display: none !important;}</style>
 </head>
-<body>
+<body ng-cloak>
 @yield('content')
 @yield('scripts')
 </body>
