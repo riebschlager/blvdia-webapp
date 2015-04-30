8f7a1f4b8737e0df99e537bf3e6aa12e4c14664d
Chris Riebschlager <riebschlager@gmail.com>
2015-04-29 21:38:13 -0500

    description

--- resources/views/photo.blade.php
+++ resources/views/photo.blade.php
@@ -3,6 +3,7 @@
 @section('og')
 <meta property="og:title" content="My BLVDIA Passport Photo" />
 <meta property="og:type" content="website" />
+<meta property="og:description" content="Hello from Boulevardia!" />
 <meta property="og:url" content="{{url('/')}}/photo/{{$photo->uid}}" />
 <meta property="og:image" content="{{$photo->url}}" />
 <meta property="og:image:user_generated" content="true">
