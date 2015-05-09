e7e01f1776100efec13554c8a2c0dc34eeba5f16
Chris Riebschlager <riebschlager@gmail.com>
2015-05-08 23:15:29 -0500

    admin

--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -2,24 +2,66 @@ angular.module('Admin', []);
 
 angular.module('Admin')
     .controller('AdminCtrl', ['$scope', '$log',
-        function ($scope, $log) {
-            var socket = io('http://blvdia.herokuapp.com:80/');
-            $scope.previewSrc = [];
-            $scope.previewSrc[0] = '';
-            $scope.previewSrc[1] = '';
-            $scope.previewSrc[2] = '';
-            $scope.previewSrc[3] = '';
-            $scope.previewSrc[4] = '';
-            $scope.previewSrc[5] = '';
-            $scope.preview = function (cameraId) {
-                socket.emit('preview', {
-                    cameraId: cameraId
-                });
-            };
-            socket.on('preview-complete', function (msg) {
-                $scope.$apply(function () {
-                    $scope.previewSrc[msg.cameraId] = msg.url;
-                });
+        function($scope, $log) {
+          var socket = io('http://blvdia.herokuapp.com:80/');
+          $scope.heartbeats = [];
+          $scope.previewSrc = [];
+          $scope.previewSrc[0] = '';
+          $scope.previewSrc[1] = '';
+          $scope.previewSrc[2] = '';
+          $scope.previewSrc[3] = '';
+          $scope.previewSrc[4] = '';
+          $scope.previewSrc[5] = '';
+
+          for (var i = 0; i < 6; i++) {
+            $scope.heartbeats[i] = {
+              current: 0,
+              previous: 0
+            }
+          }
+
+          $scope.preview = function(cameraId) {
+            socket.emit('preview', {
+              cameraId: cameraId
             });
+          };
+
+          $scope.reboot = function(cameraId) {
+            $scope.heartbeats[cameraId].current = 0;
+            $scope.heartbeats[cameraId].previous = 0;
+            socket.emit('reboot', {
+              cameraId: cameraId
+            });
+          };
+
+          $scope.shutdown = function(cameraId) {
+            socket.emit('shutdown', {
+              cameraId: cameraId
+            });
+          };
+
+          $scope.isAlive = function(cameraId) {
+            var isAlive = true;
+            if ($scope.heartbeats[cameraId].previous === $scope.heartbeats[cameraId].current) {
+              isAlive = false;
+            } else {
+              isAlive = $scope.heartbeats[cameraId].current - $scope.heartbeats[cameraId].previous < 1500;
+            }
+            return isAlive;
+          }
+
+          socket.on('preview-complete', function(msg) {
+            $scope.$apply(function() {
+              $scope.previewSrc[msg.cameraId] = msg.url;
+            });
+          });
+
+          socket.on('heartbeat', function(msg) {
+            $scope.$apply(function() {
+              $scope.heartbeats[msg.cameraId].previous = $scope.heartbeats[msg.cameraId].current;
+              $scope.heartbeats[msg.cameraId].current = msg.time;
+            });
+          });
+
         }
     ]);
--- resources/views/admin-home.blade.php
+++ resources/views/admin-home.blade.php
@@ -4,32 +4,86 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-4">
-            <button class="btn btn-block btn-sm btn-primary" ng-click="preview(0)">Camera 0</button>
+            <h3>
+                Camera 0
+                <span ng-if="isAlive(0)" style="color:#0c0;">&bull;</span>
+                <span ng-if="!isAlive(0)" style="color:#c00;">&bull;</span>
+            </h3>
+            <div class="form-group">
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(0)" ng-click="preview(0)">Preview</button>
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(0)" ng-click="reboot(0)">Reboot</button>
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(0)" ng-click="shutdown(0)">Shutdown</button>
+            </div>
             <img class="img-responsive" src="" ng-if="previewSrc[0] != ''" ng-src="@{{previewSrc[0]}}" alt="">
             <img class="img-responsive" ng-if="previewSrc[0] == ''" src="http://placehold.it/400x300&amp;text=Preview" alt="">
         </div>
         <div class="col-sm-4">
-            <button class="btn btn-block btn-sm btn-primary" ng-click="preview(1)">Camera 1</button>
+            <h3>
+                Camera 1
+                <span ng-if="isAlive(1)" style="color:#0c0;">&bull;</span>
+                <span ng-if="!isAlive(1)" style="color:#c00;">&bull;</span>
+            </h3>
+            <div class="form-group">
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(1)" ng-click="preview(1)">Preview</button>
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(1)" ng-click="reboot(1)">Reboot</button>
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(1)" ng-click="shutdown(1)">Shutdown</button>
+            </div>
             <img class="img-responsive" src="" ng-if="previewSrc[1] != ''" ng-src="@{{previewSrc[1]}}" alt="">
             <img class="img-responsive" ng-if="previewSrc[1] == ''" src="http://placehold.it/400x300&amp;text=Preview" alt="">
         </div>
         <div class="col-sm-4">
-            <button class="btn btn-block btn-sm btn-primary" ng-click="preview(2)">Camera 2</button>
+            <h3>
+                Camera 2
+                <span ng-if="isAlive(2)" style="color:#0c0;">&bull;</span>
+                <span ng-if="!isAlive(2)" style="color:#c00;">&bull;</span>
+            </h3>
+            <div class="form-group">
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(2)" ng-click="preview(2)">Preview</button>
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(2)" ng-click="reboot(2)">Reboot</button>
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(2)" ng-click="shutdown(2)">Shutdown</button>
+            </div>
             <img class="img-responsive" src="" ng-if="previewSrc[2] != ''" ng-src="@{{previewSrc[2]}}" alt="">
             <img class="img-responsive" ng-if="previewSrc[2] == ''" src="http://placehold.it/400x300&amp;text=Preview" alt="">
         </div>
         <div class="col-sm-4">
-            <button class="btn btn-block btn-sm btn-primary" ng-click="preview(3)">Camera 3</button>
+            <h3>
+                Camera 3
+                <span ng-if="isAlive(3)" style="color:#0c0;">&bull;</span>
+                <span ng-if="!isAlive(3)" style="color:#c00;">&bull;</span>
+            </h3>
+            <div class="form-group">
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(3)" ng-click="preview(3)">Preview</button>
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(3)" ng-click="reboot(3)">Reboot</button>
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(3)" ng-click="shutdown(3)">Shutdown</button>
+            </div>
             <img class="img-responsive" src="" ng-if="previewSrc[3] != ''" ng-src="@{{previewSrc[3]}}" alt="">
             <img class="img-responsive" ng-if="previewSrc[3] == ''" src="http://placehold.it/400x300&amp;text=Preview" alt="">
         </div>
         <div class="col-sm-4">
-            <button class="btn btn-block btn-sm btn-primary" ng-click="preview(4)">Camera 4</button>
+            <h3>
+                Camera 4
+                <span ng-if="isAlive(4)" style="color:#0c0;">&bull;</span>
+                <span ng-if="!isAlive(4)" style="color:#c00;">&bull;</span>
+            </h3>
+            <div class="form-group">
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(4)" ng-click="preview(4)">Preview</button>
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(4)" ng-click="reboot(4)">Reboot</button>
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(4)" ng-click="shutdown(4)">Shutdown</button>
+            </div>
             <img class="img-responsive" src="" ng-if="previewSrc[4] != ''" ng-src="@{{previewSrc[4]}}" alt="">
             <img class="img-responsive" ng-if="previewSrc[4] == ''" src="http://placehold.it/400x300&amp;text=Preview" alt="">
         </div>
         <div class="col-sm-4">
-            <button class="btn btn-block btn-sm btn-primary" ng-click="preview(5)">Camera 5</button>
+            <h3>
+                Camera 5
+                <span ng-if="isAlive(5)" style="color:#0c0;">&bull;</span>
+                <span ng-if="!isAlive(5)" style="color:#c00;">&bull;</span>
+            </h3>
+            <div class="form-group">
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(5)" ng-click="preview(5)">Preview</button>
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(5)" ng-click="reboot(5)">Reboot</button>
+                <button class="btn btn-xs btn-default" ng-disabled="!isAlive(5)" ng-click="shutdown(5)">Shutdown</button>
+            </div>
             <img class="img-responsive" src="" ng-if="previewSrc[5] != ''" ng-src="@{{previewSrc[5]}}" alt="">
             <img class="img-responsive" ng-if="previewSrc[5] == ''" src="http://placehold.it/400x300&amp;text=Preview" alt="">
         </div>
