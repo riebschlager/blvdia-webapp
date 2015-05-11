56fad7f4bccde34f2836f6f73067b83ffe2a3490
Chris Riebschlager <riebschlager@gmail.com>
2015-05-11 12:09:57 -0500

    better preview

--- resources/views/admin-home.blade.php
+++ resources/views/admin-home.blade.php
@@ -11,7 +11,7 @@
                 <span ng-if="!isAlive(0)" style="color:#c00;">&bull;</span>
             </h3>
             <img class="img-responsive" src="" ng-if="previewSrc[0] != ''" ng-src="@{{previewSrc[0]}}" alt="">
-            <img class="img-responsive" ng-if="previewSrc[0] == ''" src="http://placehold.it/400x300&amp;text=Preview" alt="">
+            <img class="img-responsive" ng-if="previewSrc[0] == ''" src="https://s3-us-west-2.amazonaws.com/blvdia-preview/camera-0.jpg" alt="">
             <div style="margin-top: 0.5em;">
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(0)" ng-click="preview(0)">Preview</button>
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(0)" ng-click="reboot(0)">Reboot</button>
@@ -25,7 +25,7 @@
                 <span ng-if="!isAlive(1)" style="color:#c00;">&bull;</span>
             </h3>
             <img class="img-responsive" src="" ng-if="previewSrc[1] != ''" ng-src="@{{previewSrc[1]}}" alt="">
-            <img class="img-responsive" ng-if="previewSrc[1] == ''" src="http://placehold.it/400x300&amp;text=Preview" alt="">
+            <img class="img-responsive" ng-if="previewSrc[1] == ''" src="https://s3-us-west-2.amazonaws.com/blvdia-preview/camera-1.jpg" alt="">
             <div style="margin-top: 0.5em;">
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(1)" ng-click="preview(1)">Preview</button>
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(1)" ng-click="reboot(1)">Reboot</button>
@@ -39,7 +39,7 @@
                 <span ng-if="!isAlive(2)" style="color:#c00;">&bull;</span>
             </h3>
             <img class="img-responsive" src="" ng-if="previewSrc[2] != ''" ng-src="@{{previewSrc[2]}}" alt="">
-            <img class="img-responsive" ng-if="previewSrc[2] == ''" src="http://placehold.it/400x300&amp;text=Preview" alt="">
+            <img class="img-responsive" ng-if="previewSrc[2] == ''" src="https://s3-us-west-2.amazonaws.com/blvdia-preview/camera-2.jpg" alt="">
             <div style="margin-top: 0.5em;">
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(2)" ng-click="preview(2)">Preview</button>
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(2)" ng-click="reboot(2)">Reboot</button>
@@ -53,7 +53,7 @@
                 <span ng-if="!isAlive(3)" style="color:#c00;">&bull;</span>
             </h3>
             <img class="img-responsive" src="" ng-if="previewSrc[3] != ''" ng-src="@{{previewSrc[3]}}" alt="">
-            <img class="img-responsive" ng-if="previewSrc[3] == ''" src="http://placehold.it/400x300&amp;text=Preview" alt="">
+            <img class="img-responsive" ng-if="previewSrc[3] == ''" src="https://s3-us-west-2.amazonaws.com/blvdia-preview/camera-3.jpg" alt="">
             <div style="margin-top: 0.5em;">
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(3)" ng-click="preview(3)">Preview</button>
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(3)" ng-click="reboot(3)">Reboot</button>
@@ -67,7 +67,7 @@
                 <span ng-if="!isAlive(4)" style="color:#c00;">&bull;</span>
             </h3>
             <img class="img-responsive" src="" ng-if="previewSrc[4] != ''" ng-src="@{{previewSrc[4]}}" alt="">
-            <img class="img-responsive" ng-if="previewSrc[4] == ''" src="http://placehold.it/400x300&amp;text=Preview" alt="">
+            <img class="img-responsive" ng-if="previewSrc[4] == ''" src="https://s3-us-west-2.amazonaws.com/blvdia-preview/camera-4.jpg" alt="">
             <div style="margin-top: 0.5em;">
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(4)" ng-click="preview(4)">Preview</button>
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(4)" ng-click="reboot(4)">Reboot</button>
@@ -81,7 +81,7 @@
                 <span ng-if="!isAlive(5)" style="color:#c00;">&bull;</span>
             </h3>
             <img class="img-responsive" src="" ng-if="previewSrc[5] != ''" ng-src="@{{previewSrc[5]}}" alt="">
-            <img class="img-responsive" ng-if="previewSrc[5] == ''" src="http://placehold.it/400x300&amp;text=Preview" alt="">
+            <img class="img-responsive" ng-if="previewSrc[5] == ''" src="https://s3-us-west-2.amazonaws.com/blvdia-preview/camera-5.jpg" alt="">
             <div style="margin-top: 0.5em;">
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(5)" ng-click="preview(5)">Preview</button>
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(5)" ng-click="reboot(5)">Reboot</button>
