e725e915141a79dfa1c6f06d3047ffb81fdc12d6
Chris Riebschlager <riebschlager@gmail.com>
2015-05-31 10:10:22 -0500

    stupd

--- resources/views/admin-home.blade.php
+++ resources/views/admin-home.blade.php
@@ -10,7 +10,7 @@
                 <span ng-if="isAlive(0)" style="color:#0c0;">&bull;</span>
                 <span ng-if="!isAlive(0)" style="color:#c00;">&bull;</span>
             </h3>
-            <img class="img-responsive" ng-src="previewSrc[0]" src="http://placehold.it/640x480&amp;text=BLVDIA" alt="">
+            <img class="img-responsive" ng-src="@{{previewSrc[0]}}" src="http://placehold.it/640x480&amp;text=BLVDIA" alt="">
             <div style="margin-top: 0.5em;">
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(0)" ng-click="preview(0)">Preview</button>
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(0)" ng-click="reboot(0)">Reboot</button>
@@ -23,7 +23,7 @@
                 <span ng-if="isAlive(1)" style="color:#0c0;">&bull;</span>
                 <span ng-if="!isAlive(1)" style="color:#c00;">&bull;</span>
             </h3>
-            <img class="img-responsive" ng-src="previewSrc[1]" src="http://placehold.it/640x480&amp;text=BLVDIA" alt="">
+            <img class="img-responsive" ng-src="@{{previewSrc[1]}}" src="http://placehold.it/640x480&amp;text=BLVDIA" alt="">
             <div style="margin-top: 0.5em;">
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(1)" ng-click="preview(1)">Preview</button>
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(1)" ng-click="reboot(1)">Reboot</button>
@@ -36,7 +36,7 @@
                 <span ng-if="isAlive(2)" style="color:#0c0;">&bull;</span>
                 <span ng-if="!isAlive(2)" style="color:#c00;">&bull;</span>
             </h3>
-            <img class="img-responsive" ng-src="previewSrc[2]" src="http://placehold.it/640x480&amp;text=BLVDIA" alt="">
+            <img class="img-responsive" ng-src="@{{previewSrc[2]}}" src="http://placehold.it/640x480&amp;text=BLVDIA" alt="">
             <div style="margin-top: 0.5em;">
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(2)" ng-click="preview(2)">Preview</button>
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(2)" ng-click="reboot(2)">Reboot</button>
@@ -49,7 +49,7 @@
                 <span ng-if="isAlive(3)" style="color:#0c0;">&bull;</span>
                 <span ng-if="!isAlive(3)" style="color:#c00;">&bull;</span>
             </h3>
-            <img class="img-responsive" ng-src="previewSrc[3]" src="http://placehold.it/640x480&amp;text=BLVDIA" alt="">
+            <img class="img-responsive" ng-src="@{{previewSrc[3]}}" src="http://placehold.it/640x480&amp;text=BLVDIA" alt="">
             <div style="margin-top: 0.5em;">
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(3)" ng-click="preview(3)">Preview</button>
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(3)" ng-click="reboot(3)">Reboot</button>
@@ -62,7 +62,7 @@
                 <span ng-if="isAlive(4)" style="color:#0c0;">&bull;</span>
                 <span ng-if="!isAlive(4)" style="color:#c00;">&bull;</span>
             </h3>
-            <img class="img-responsive" ng-src="previewSrc[4]" src="http://placehold.it/640x480&amp;text=BLVDIA" alt="">
+            <img class="img-responsive" ng-src="@{{previewSrc[4]}}" src="http://placehold.it/640x480&amp;text=BLVDIA" alt="">
             <div style="margin-top: 0.5em;">
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(4)" ng-click="preview(4)">Preview</button>
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(4)" ng-click="reboot(4)">Reboot</button>
@@ -75,7 +75,7 @@
                 <span ng-if="isAlive(5)" style="color:#0c0;">&bull;</span>
                 <span ng-if="!isAlive(5)" style="color:#c00;">&bull;</span>
             </h3>
-            <img class="img-responsive" ng-src="{{previewSrc[5]}}" src="http://placehold.it/640x480&amp;text=BLVDIA" alt="">
+            <img class="img-responsive" ng-src="@{{previewSrc[5]}}" src="http://placehold.it/640x480&amp;text=BLVDIA" alt="">
             <div style="margin-top: 0.5em;">
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(5)" ng-click="preview(5)">Preview</button>
                 <button class="btn btn-xs btn-default" ng-disabled="!isAlive(5)" ng-click="reboot(5)">Reboot</button>
