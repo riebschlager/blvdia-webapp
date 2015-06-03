c8d6f951ef354df4410984eb5a43d77bc28336b4
Chris Riebschlager <riebschlager@gmail.com>
2015-06-02 21:40:50 -0500

    ugh

--- resources/views/photo.blade.php
+++ resources/views/photo.blade.php
@@ -15,7 +15,7 @@
 <meta property="og:image:type" content="image/gif">
 <meta property="og:site_name" content="Boulevardia">
 
-<meta name="twitter:card" content="player" />
+<meta name="twitter:card" content="photo" />
 <meta name="twitter:site" content="@blvdia" />
 <meta name="twitter:title" content="Hello from Boulevardia!" />
 <meta name="twitter:image" content="{{$photo->url}}" />
